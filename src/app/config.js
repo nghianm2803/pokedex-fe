0481351d95a6e0b03f3fa4537de818bf0b5a3297
nghia <nghianm2803@gmail.com>
2023-06-14 00:17:31 +0700

    update base url

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,4 @@
 // export const BASE_URL = process.env.REACT_APP_BACKEND_API;
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://pokedex-doo.onrender.com/";
 export const POKEMONS_PER_PAGE = 20;
